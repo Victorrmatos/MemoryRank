d5ed8aa9f95cd784514e416e8a870ea3cb1d989c
Victor Reis Matos <97291820+Victorrmatos@users.noreply.github.com>
2023-03-21 20:58:03 +0000

    formatting

--- firebase.config.js
+++ firebase.config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBmeQ4jqhpTHKNZ5oVsCzoSBHodOtQox30",
-    authDomain: "memoryrank.firebaseapp.com",
-    databaseURL: "https://memoryrank-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "memoryrank",
-    storageBucket: "memoryrank.appspot.com",
-    messagingSenderId: "325839389976",
-    appId: "1:325839389976:web:d175421e85563e41320a6d",
-    measurementId: "G-V1V5QKBD7G"
-  };
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBmeQ4jqhpTHKNZ5oVsCzoSBHodOtQox30",
+  authDomain: "memoryrank.firebaseapp.com",
+  databaseURL:
+    "https://memoryrank-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "memoryrank",
+  storageBucket: "memoryrank.appspot.com",
+  messagingSenderId: "325839389976",
+  appId: "1:325839389976:web:d175421e85563e41320a6d",
+  measurementId: "G-V1V5QKBD7G",
+};
+firebase.initializeApp(firebaseConfig);
